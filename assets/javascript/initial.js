781db626fa1962657751249bbf7a2eb7cde85758
Yuwen Li <muzlee1113@gmail.com>
2019-02-10 12:08:37 -0600

    test sign in

--- assets/javascript/initial.js
+++ assets/javascript/initial.js
@@ -15,29 +15,35 @@ firebase.initializeApp(config);
 var database = firebase.database();
 
 //authentication
-$("#signInWithGoogle").on("click", function signInWithGoogle() {
-    console.log("click")
-    var provider = new firebase.auth.GoogleAuthProvider();
-    provider.addScope('profile');
-    provider.addScope('email');
+var provider = new firebase.auth.GoogleAuthProvider();
 
-    return firebase.auth().signInWithPopup(provider).then(function(result) {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        var token = result.credential.accessToken;
-        // The signed-in user info.
-        var user = result.user;
-        // ...
-      }).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        // ...
-      });
-})
+        function googleSignin() {
+           firebase.auth()
+
+           .signInWithPopup(provider).then(function(result) {
+              var token = result.credential.accessToken;
+              var user = result.user;
+
+              console.log(token)
+              console.log(user)
+           }).catch(function(error) {
+              var errorCode = error.code;
+              var errorMessage = error.message;
+
+              console.log(error.code)
+              console.log(error.message)
+           });
+        }
+
+        function googleSignout() {
+           firebase.auth().signOut()
+
+           .then(function() {
+              console.log('Signout Succesfull')
+           }, function(error) {
+              console.log('Signout Failed')  
+           });
+        }
 
 
 
